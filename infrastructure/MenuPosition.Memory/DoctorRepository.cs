58c60cbf8bedb72f9a07dea4cebeb10ba8b38e6c
Evgeny <you@example.com>
2022-04-22 08:41:38 +0300

    Создать веб приложение

--- infrastructure/MenuPosition.Memory/DoctorRepository.cs
+++ infrastructure/MenuPosition.Memory/DoctorRepository.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace MenuPosition.Memory
 {
-    public class DoctorRepository
+    public class DoctorRepository:IDoctorRepository
     {
+        private readonly Doctor[] doctors = new[]
+        {
+new Doctor(1,"Хирург Павел Игнатович Чеччер"),
+new Doctor(2,"Акушер Василий Игнатович Булава"),
+new Doctor(2,"Окулист Анастасия Сергеевна Лапочка"),
+        };
+
+        public Doctor[] GetAllByName(string namePart)
+        {
+            return doctors.Where(doctors => doctors.Name.Contains(namePart))
+                            .ToArray();
+                }
     }
 }
